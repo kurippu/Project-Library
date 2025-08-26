2262057bdda475ef4daa5ba9f39aae90a072eb2a
angle <frenfrieking@gmail.com>
2025-08-26 15:00:50 -0700

    add main home and js and try add goodle verification

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function submission(){
+
+let book = {
+    author: document.getElementById("author").value,
+    title : document.getElementById("title").value,
+    isbn : document.getElementById("isbn").value
+
+}
+console.log(book)
+}
+
+
+
+const myLibrary = [];
+
+function Book(book) {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
